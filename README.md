4fd893aab788a06791a50aeecb0006c67610a6d9
minyan.he <hmy990528@gmail.com>
2024-07-03 10:29:24 +0800

    [master]统一异常处理和文档补充

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # currency
 代币系统
+设计文档：https://c7nxmfumo3.feishu.cn/docx/TEFrdTtEhoX7uUxjkg7ccKnLnbg?from=from_copylink
+接口文档：https://c7nxmfumo3.feishu.cn/docx/R7ZWdNjcho6pItxwFG2c13oKnId?from=from_copylink
--- currency-capi/src/main/java/com/minyan/currencycapi/aspect/CustomExceptionHandler.java
+++ currency-capi/src/main/java/com/minyan/currencycapi/aspect/CustomExceptionHandler.java
@@ -6,16 +6,18 @@ import com.minyan.vo.ApiResult;
 import java.lang.reflect.UndeclaredThrowableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @decription 异常处理切面
  * @author minyan.he
  * @date 2024/7/1 11:45
  */
-//@ControllerAdvice
+@ControllerAdvice
 public class CustomExceptionHandler {
 
   // 处理自定义异常类型
+  @ResponseBody
   @ExceptionHandler(value = {UndeclaredThrowableException.class})
   public ApiResult<?> handleCustomException(UndeclaredThrowableException ex) {
     Throwable undeclaredThrowable = ex.getUndeclaredThrowable();
@@ -28,6 +30,7 @@ public class CustomExceptionHandler {
   }
 
   // 处理其他未被捕获的异常
+  @ResponseBody
   @ExceptionHandler(value = {Exception.class})
   public ApiResult<?> handleGeneralException(Exception ex) {
     return new ApiResult<>(CodeEnum.FAIL.getCode(), ex.getMessage(), null);
