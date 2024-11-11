151e9cca5809d6a562b04fb32b71160c10e196a6
minyan.he <hmy990528@gmail.com>
2024-11-11 20:48:50 +0900

    [master]自定义异常优化

--- currency-capi/src/main/java/com/minyan/currencycapi/aspect/CustomExceptionHandler.java
+++ currency-capi/src/main/java/com/minyan/currencycapi/aspect/CustomExceptionHandler.java
@@ -4,22 +4,29 @@ import com.minyan.Enum.CodeEnum;
 import com.minyan.exception.CustomException;
 import com.minyan.vo.ApiResult;
 import java.lang.reflect.UndeclaredThrowableException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  * @decription 异常处理切面
  * @author minyan.he
  * @date 2024/7/1 11:45
  */
-@ControllerAdvice
+@RestControllerAdvice
 public class CustomExceptionHandler {
 
   // 处理自定义异常类型
   @ResponseBody
-  @ExceptionHandler(value = {UndeclaredThrowableException.class})
-  public ApiResult<?> handleCustomException(UndeclaredThrowableException ex) {
+  @ExceptionHandler(CustomException.class)
+  public ApiResult<?> handleCustomException(CustomException customException) {
+    return new ApiResult<>(customException.getCode(), customException.getMessage(), null);
+  }
+
+  // 处理自定义异常类型
+  @ResponseBody
+  @ExceptionHandler(UndeclaredThrowableException.class)
+  public ApiResult<?> handleOtherException(UndeclaredThrowableException ex) {
     Throwable undeclaredThrowable = ex.getUndeclaredThrowable();
     if (undeclaredThrowable instanceof CustomException) {
       CustomException customException = (CustomException) undeclaredThrowable;
@@ -31,7 +38,7 @@ public class CustomExceptionHandler {
 
   // 处理其他未被捕获的异常
   @ResponseBody
-  @ExceptionHandler(value = {Exception.class})
+  @ExceptionHandler(Exception.class)
   public ApiResult<?> handleGeneralException(Exception ex) {
     return new ApiResult<>(CodeEnum.FAIL.getCode(), ex.getMessage(), null);
   }
